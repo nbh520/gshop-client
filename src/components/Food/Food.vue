16f17e2220d3854efaa940e097c683d05dc2b8df
nibohan <154524331@qq.com>
2018-10-29 17:52:19 +0800

    1029

--- src/components/Food/Food.vue
+++ src/components/Food/Food.vue
@@ -5,16 +5,47 @@
 			<div class="image-header">
 				<img src="" alt="">
 				<p class="foodpanel-desc"></p>
-				<div class="back"></div>
+				<div class="back">
+					<i class="iconfont icon-arrow_left"></i>
+				</div>
+			</div>
+			<div class="content">
+				<h1 class="title"></h1>
+				<div class="detail">
+					<span class="sell-count"></span>
+					<span class="rating"></span>
+				</div>
+				<div class="price">
+					<span class="now"></span>
+					<span class="old"></span>
+				</div>
+				<div class="cartcontrol-wrapper">
+					<CartControl :food="food"></CartControl>
+				</div>
 			</div>
 		</div>
+	  <div class="food-cover"></div>
   </div>
 </template>
 
 <script>
+import CartControl from "../CartControl/CartControl.vue";
 export default {
   data() {
-    return {};
+    return {
+      isShow: false
+    };
+  },
+  props: {
+    food: Object
+  },
+  methods: {
+    toggleShow() {
+      this.isShow = !this.isShow;
+    }
+  },
+  components: {
+    CartControl
   }
 };
 </script>
--- src/pages/Shop/ShopGoods/ShopGoods.vue
+++ src/pages/Shop/ShopGoods/ShopGoods.vue
@@ -22,7 +22,7 @@
 					<li class="food-list-hook" v-for="(good, index) in goods" :key="index">
 						<h1 class="title">{{good.name}}</h1>
 						<ul>
-							<li class="food-item bottom-border-1px" v-for="(food, index) in good.foods" :key="index">
+							<li class="food-item bottom-border-1px" v-for="(food, index) in good.foods" :key="index" @click="showFood(food)">
 								<div class="icon">
 									<img width="57" height="57" :src="food.icon">
 								</div>
@@ -47,12 +47,16 @@
 				</ul>
 			</div>
 		</div>
-  </div> 
+		<Food :food="food" ref="food"></Food>
+	
+	
+	</div> 
 </template>
 
 <script>
 import { mapState } from "vuex";
 import BScroll from "better-scroll";
+import Food from "../../../components/Food/Food.vue";
 export default {
   data() {
     return {
@@ -129,7 +133,17 @@ export default {
       this.scrollY = scrollY;
       //平滑滑动右侧列表 better-scroll里的方法
       this.foodsScroll.scrollTo(0, -scrollY, 300);
+    },
+    //显示点击的food
+    showFood(food) {
+      //设置要传递给food的组件的数据
+      this.food = food;
+      //显示food组件(在父组件中调用子组件对象的方法)
+      this.$refs.food.toggleShow();
     }
+  },
+  components: {
+    Food
   }
 };
 </script>
